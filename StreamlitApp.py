779c8129eeaabcba94ea2d125b9e0e3c662ee84a
hailstorm26 <hstouder@ttu.edu>
2022-12-01 15:01:10 -0600

    log in (3rd time's a charm?)

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -2,30 +2,38 @@ import streamlit as st
 import pickle
 from pathlib import Path
 import streamlit_authenticator as stauth
+import yaml
+from streamlit_authenticator import Authenticate
+from yaml import SafeLoader
 
 st.set_page_config(page_title="Movie Booking System", page_icon=":movie_camera:", layout="wide")
 
-names = ["Peter Parker", "Rebecca Miller"]
-usernames = ["pparker", "rmiller"]
-passwords = ["pw1", "pw2"]
-hashed_passwords = stauth.Hasher(passwords).generate()
 
 # log in
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-    "sales_dashboard", "abcdef", cookie_expiry_days=0)
-name, authentication_status, username = authenticator.login("Login", "main")
+hashed_passwords = stauth.Hasher(['123', '456']).generate()
+
+with open('credentials.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
 if not authentication_status:
     st.error("username or password is incorrect")
 if authentication_status is None:
-    st.error("Please enter your username and passowrd")
+    st.warning("Please enter your username and password")
 
 # home page
 if authentication_status:
-    authenticator.logout("Logout", "sidebar")
-    st.sidebar.title("Your account")
-
+    authenticator.logout("Logout", "main")
     st.title("Home")
     st.button("Search Movies")
     st.button("Browse Current Movie Catalog")
     st.button("Browse Upcoming Movie Catalog")
-
