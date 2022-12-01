a4739566d926179acef1764658e8e093fe06c702
hailstorm26 <hstouder@ttu.edu>
2022-12-01 14:30:42 -0600

    log in (take 2 lol)

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,24 +1,26 @@
 import streamlit as st
+import pickle
+from pathlib import Path
 import streamlit_authenticator as stauth
 
 st.set_page_config(page_title="Movie Booking System", page_icon=":movie_camera:", layout="wide")
 
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+passwords = ["pw1", "pw2"]
+hashed_passwords = stauth.Hasher(passwords).generate()
+
 # log in
-usernames = ["user1", "user2", "user3"]
-passwords = ["pw111", "pw222", "pw333"]
-
-authenticator = stauth.Authenticate(usernames, passwords, "dashboard", "abcdef", cookie_expiry_days=30)
-
-username, authentication_status = authenticator.login("Login", "main")
-
-if authentication_status == False:
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=0)
+name, authentication_status, username = authenticator.login("Login", "main")
+if not authentication_status:
     st.error("username or password is incorrect")
-
-if authentication_status == None:
+if authentication_status is None:
     st.error("Please enter your username and passowrd")
 
 # home page
-if authentication_status == True:
+if authentication_status:
     authenticator.logout("Logout", "sidebar")
     st.sidebar.title("Your account")
 
