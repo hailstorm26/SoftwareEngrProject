3135c7fc11e6056a587d5daf897eff75c1bd4dd8
hailstorm26 <hstouder@ttu.edu>
2022-12-01 13:20:18 -0600

    initial streamlit code

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,2 +1,9 @@
-if __name__=="__main__":
-    print("hello world")
+import streamlit as st
+
+st.title("Movie Booking System")
+st.markdown("this is the markdown")
+st.header("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.button("log in")
+st.button("sign up")
